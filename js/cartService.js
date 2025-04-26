38f1c8f3c74342f692f5b8ced63c17992b525cfe
Johan Giraldo Duque <johan.dgiraldo@gmail.com>
2025-04-26 12:18:05 -0500

    Carrito de compra, suscripciones

--- js/cartService.js
+++ js/cartService.js
@@ -1,35 +1,37 @@
-function agregarCarrito(producto){
+function agregarAlCarrito(producto){
     const memoria =JSON.parse(localStorage.getItem("suscripciones"));
     console.log(memoria);
-    if(memoria){
+    if(!memoria){
         const nuevoProducto = getNuevoproductoParaMemoria (producto);
         nuevoProducto.cantidad=1;
-        localStorage.setItem("suscripciones",JSON.stringify[nuevoProducto])
+        localStorage.setItem("suscripciones",JSON.stringify([nuevoProducto]));
     }else{
-        const indiceProducto=memoria.findIndex(suscripcion =>suscripcion.id==producto.id)
-        console.log(indiceProducto)
+        const indiceProducto=memoria.findIndex(suscripcion =>suscripcion.id==producto.id);
+        console.log(indiceProducto);
+        const nuevaMemoria = memoria;
         if(indiceProducto== -1){
-            const nuevaMemoria = memoria;
             nuevaMemoria.push(getNuevoproductoParaMemoria(producto))
-            localStorage.setItem("suscripciones",JSON.stringify[nuevaMemoria]);
+            localStorage.setItem("suscripciones",JSON.stringify(nuevaMemoria));
         }else{
-            nuevaMemoria[indiceProducto].cantidad++;
+            nuevaMemoria[indiceProducto].cantidad ++;
         }
+        localStorage.setItem("suscripciones",JSON.stringify(nuevaMemoria));
     }
+    actualizarNumeroCarrito();
 }
 // toma un producto, le agrega cantidad 1 y lo devuelve
 function getNuevoproductoParaMemoria(producto){
     const nuevoProducto = producto;
-    producto.cantidad  = 1;
+    nuevoProducto.cantidad  = 1;
     return nuevoProducto;
     
 }
 
-const cuentaCarritoElement = document.getElementById("cuenta-carrito")
+const cuentaCarritoElement = document.getElementById("cuenta-carrito");
 function actualizarNumeroCarrito(){
     const memoria =JSON.parse(localStorage.getItem("suscripciones"));
     const cuenta = memoria.reduce((acum, current)=> acum+current.cantidad,0);
-    cuentaCarritoElement.innerHTML=cuenta;
-
-
+    cuentaCarritoElement.innerText = cuenta;
 }
+
+actualizarNumeroCarrito();
