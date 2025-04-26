5e252a1e57bb67e8dd2877046a6926db8dcdd86f
Johan Giraldo Duque <johan.dgiraldo@gmail.com>
2025-04-26 00:03:38 -0500

    Cree la pagina para agregar al carrito de compras las suscripciones para la pagina de contrapunto news

---
+++ js/suscripciones.js
@@ -0,0 +1,16 @@
+const suscripciones =[
+    {
+        id:1,
+        nombre: "Básico",
+        img : "./img/productos/1.png",
+        precio: 80,
+    },
+
+
+    {
+        id:2,
+        nombre: "Premiun",
+        img : "./img/productos/2.png",
+        precio: 180,
+    }
+]
