c284e5b52faf774b300af1559512865be889ecd1
Johan Giraldo Duque <johan.dgiraldo@gmail.com>
2025-04-29 00:14:16 -0500

    corrigiendo errores

--- index.js
+++ index.js
@@ -116,9 +116,9 @@ const showHTML = () => {
         `;
 
 		rowProduct.append(containerProduct);
-
+		
 		total =
-			total + parseFloat(product.quantity * product.price.slice(1));
+			total + product.quantity * parseInt( product.price.slice(1).replace(".",""));
 		totalOfProducts = totalOfProducts + product.quantity;
 	});
 
